4a0a439f6a762807dc3dc55506509588a7b81e6c
nikita-silent <npcweb@mail.ru>
2019-10-22 13:29:35 +0700

    Add TODO and input

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 # Case-study #2
 # Developers:   Drachev Nikita (),
 #               Starnovskiy Sergey (),
-#               Zhuravlev Alexander ()
-# Programm calculate hight inerest rate
+#               Zhuravlev Alexander ().
+# Program calculate compound interest rate with investment during the each month
+
+import local as lc
+years = int(input("Срок размещения капитала (лет):"))
+initial_capital = float(input("Начальный капитал ($):"))
+percent = float(input("Процентная ставка (%/мес.):"))
+investment_infusion = float(input("Инвестиционные вливания ($/мес.):"))
+for year in range(1, years + 1):
+    print(year, "год")
+    print("-------------------------------------------")
+    print("|       |   основа   | сумма %  |         |")
+    print("| месяц | инвестиций | за месяц | капитал |")
+    print("-------------------------------------------")
+# TODO: Make formula for compound interest rate
+pass
+# TODO: Make network for formula
+pass
+# TODO: Make loop for network
+pass
