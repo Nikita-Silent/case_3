c1a36d28833dc084a35df9de5a45fea18abad8f7
sstarnovskii <kopernik.t.800@gmail.com>
2019-10-23 22:01:21 +0700

    updated local.py updated input section

--- main.py
+++ main.py
@@ -1,23 +1,42 @@
-# Case-study #2
+# Case-study #3
 # Developers:   Drachev Nikita (),
 #               Starnovskiy Sergey (),
 #               Zhuravlev Alexander ().
-# Program calculate compound interest rate with investment during the each month
+# Program calculates compound interest with investment during each month and displays it in tables.
 
 import local as lc
-years = int(input("Срок размещения капитала (лет):"))
-initial_capital = float(input("Начальный капитал ($):"))
-percent = float(input("Процентная ставка (%/мес.):"))
-investment_infusion = float(input("Инвестиционные вливания ($/мес.):"))
-for year in range(1, years + 1):
-    print(year, "год")
-    print("-------------------------------------------")
-    print("|       |   основа   | сумма %  |         |")
-    print("| месяц | инвестиций | за месяц | капитал |")
-    print("-------------------------------------------")
+
+years = float(input(lc.YRS))
+while years <= 0 or years != int(years):
+    print(lc.ERR_1)
+    years = float(input(lc.YRS))
+years = int(years)
+
+initial_capital = float(input(lc.INIT_CAP))
+while initial_capital <= 0:
+    print(lc.ERR_2)
+    initial_capital = float(input(lc.INIT_CAP))
+
+interest_rate = float(input(lc.INT_RATE))
+while interest_rate <= 0:
+    print(lc.ERR_3)
+    interest_rate = float(input(lc.INT_RATE))
+
+investment_injection = float(input(lc.INV_INJ))
+while investment_injection < 0:
+    print(lc.ERR_4)
+    investment_injection = float(input(lc.INV_INJ))
+
 # TODO: Make formula for compound interest rate
 pass
 # TODO: Make network for formula
 pass
 # TODO: Make loop for network
 pass
+
+for year in range(1, years + 1):
+    print(year, lc.YEAR)
+    print("-------------------------------------------")
+    print("|       |   {}   | {}  |         |".format(lc.BASE, lc.SUM))
+    print("| {} | {} | {} | {} |".format(lc.MONTH, lc.INVESTMENTS, lc.F_MON, lc.CAP))
+    print("-------------------------------------------")
